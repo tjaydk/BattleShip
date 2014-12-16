fa26d18c9d726142627baa5b4c67fc281fd09cd3
tjaydk <dennis_michael@jubii.dk>
2014-12-16 16:07:56 +0100

    Changes to shootingsequence

--- BattleShip_Trafalgar/src/y42/Y42.java
+++ BattleShip_Trafalgar/src/y42/Y42.java
@@ -16,116 +16,135 @@ import java.util.Random;
  *
  * @author Tobias
  */
-public class Y42 implements BattleshipsPlayer
-{
+public class Y42 implements BattleshipsPlayer {
+
     private final static SecureRandom rnd = new SecureRandom();
     private int sizeX;
     private int sizeY;
     private int x, y;
+    private int shotSeq;
+    private boolean hit;
+    private boolean[][] shotsFired;
 
-    public Y42()
-    {
+    public Y42() {
     }
 
     @Override
-    public void placeShips(Fleet fleet, Board board)
-    {
-         sizeX = board.sizeX();
+    public void placeShips(Fleet fleet, Board board) {
+        sizeX = board.sizeX();
         sizeY = board.sizeY();
         for (int i = 0; i < fleet.getNumberOfShips(); ++i) {
             Ship s = fleet.getShip(i);
             boolean vertical;
             Position pos;
-            
+
             if (i == 0) {
                 vertical = true;
-                pos = new Position(3,8);
+                pos = new Position(3, 8);
                 board.placeShip(pos, s, vertical);
             }
             if (i == 1) {
                 vertical = false;
-                pos = new Position(7,4);
+                pos = new Position(7, 4);
                 board.placeShip(pos, s, vertical);
             }
             if (i == 2) {
                 vertical = false;
-                pos = new Position(5,8);
+                pos = new Position(5, 8);
                 board.placeShip(pos, s, vertical);
             }
             if (i == 3) {
                 vertical = false;
-                pos = new Position(4,1);
+                pos = new Position(4, 1);
                 board.placeShip(pos, s, vertical);
             }
             if (i == 4) {
                 vertical = true;
-                pos = new Position(1,5);
+                pos = new Position(1, 5);
                 board.placeShip(pos, s, vertical);
             }
         }
     }
 
     @Override
-    public void incoming(Position pos)
-    {
-        
+    public void incoming(Position pos) {
+
         //Do nothing
     }
 
     @Override
-    public Position getFireCoordinates(Fleet enemyShips)
-    {
+    public Position getFireCoordinates(Fleet enemyShips) {
+        shotSeq = 0;
+
         while (enemyShips.getNumberOfShips() != 0) {
-            if (x < sizeX) {
-                for (int i = 0; i < sizeX; i++) {
-                    x = i;
-                    y = i;
-                    return new Position(x, y);
-                }
-            } else if (x == sizeX) {
-                for (int i = 0; x < 0; i++) {
-                    x = sizeX - i;
-                    y = i;
-                    if ((x == 5 && y == 4) || (x == 4 && y == 5)) {
-                        x = x - 2;
-                        y = y + 2;
-                    }
-                    return new Position(x, y);
-                }
+
+            shotsFired = new boolean[sizeX][sizeY];
+
+            if (shotSeq == 0) {
+                fireModeOne();
+            } else if (shotSeq == 1) {
+                fireModeTwo();
             }
 
         }
 
         return new Position(x, y);
     }
+    
+    public Position fireModeOne() {
+        for (int i = 0; i < sizeX; i++) {
+                    x = i;
+                    y = i;
+                    if (x == sizeX && y == sizeY) {
+                        shotSeq++;
+                    }
+                    shotsFired[i][i] = true;
+                }
+                    return new Position(x, y);
+    }
+    
+    public Position fireModeTwo() {
+        for (int i = 0; x < 0; i++) {
+                    x = sizeX - i;
+                    y = i;
+                    if ((x == 5 && y == 4) || (x == 4 && y == 5)) {
+                        x = x - 2;
+                        y = y + 2;
+                    }
+                    if (x == 0 && y == sizeY) {
+                        shotSeq++;
+                    }
+                    shotsFired[i][i] = true;
+                }
+                    return new Position(x, y);
+    }
+    
+    public Position shipHit() {
+        return new Position(x,y);
+    }
 
     @Override
-    public void hitFeedBack(boolean hit, Fleet enemyShips)
-    {
-        //Do nothing
-    }    
+    public void hitFeedBack(boolean hit, Fleet enemyShips) {
+        this.hit = hit;
+    }
 
     @Override
-    public void startMatch(int rounds)
-    {
+    public void startMatch(int rounds) {
         //Do nothing
     }
 
     @Override
-    public void startRound(int round)
-    {
+    public void startRound(int round) {
         //Do nothing
     }
 
     @Override
-    public void endRound(int round, int points, int enemyPoints)
-    {
+    public void endRound(int round, int points, int enemyPoints) {
         //Do nothing
     }
 
     @Override
-    public void endMatch(int won, int lost, int draw)
-    {
+    public void endMatch(int won, int lost, int draw) {
         //Do nothing
     }
 }
